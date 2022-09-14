4b2cc39f53ebc9f2605f911ecf523b67ce131a62
ward <bonuswon@gmail.com>
2022-09-14 17:28:26 +0900

    feat: create new server

---
+++ server/mmz/src/main/java/run/ward/mmz/MmzApplication.java
@@ -0,0 +1,13 @@
+package run.ward.mmz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmzApplication.class, args);
+    }
+
+}
---
+++ server/mmz/src/test/java/run/ward/mmz/MmzApplicationTests.java
@@ -0,0 +1,13 @@
+package run.ward.mmz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
