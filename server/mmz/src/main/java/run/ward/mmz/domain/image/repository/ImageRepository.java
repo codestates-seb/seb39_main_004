3784c5d6da7b75ac268a5853b5d21d0f51cbe521
wonyongLee <104341003+ItsWard@users.noreply.github.com>
2022-09-15 11:34:21 +0900

    Revert "Feat/be/05 image model"

--- server/mmz/src/main/java/run/ward/mmz/domain/image/repository/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package run.ward.mmz.domain.image.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import run.ward.mmz.domain.image.entity.Image;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
