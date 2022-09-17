00629c722f27e72751474c6c885e6294187cf405
wonyongLee <104341003+ItsWard@users.noreply.github.com>
2022-09-18 03:15:55 +0900

    feat: import requestMapping

--- server/src/main/java/run/ward/mmz/web/ImageController.java
+++ server/src/main/java/run/ward/mmz/web/ImageController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 import run.ward.mmz.service.image.ImageService;
