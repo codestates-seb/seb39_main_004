23ede917d7a7d55d0cd3548f6785286b47ed4bb8
JoeunNAL <euji42@gmail.com>
2022-09-22 01:34:00 +0900

    feat: SortButtons 컴포넌트 생성(#14)

--- client/src/components/CommonUI/index.ts
+++ client/src/components/CommonUI/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as StyledLink } from "./StyledLink";
 export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
+export { default as SortButtons } from "./SortButtons";
