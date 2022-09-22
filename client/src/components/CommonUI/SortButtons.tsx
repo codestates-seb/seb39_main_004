08b4efec7cbe3ddc33a4f350c07dd8965b2a1c83
JoeunNAL <euji42@gmail.com>
2022-09-22 16:54:11 +0900

    feat: SortButtons 클릭이벤트 적용(#14)

--- client/src/components/CommonUI/SortButtons.tsx
+++ client/src/components/CommonUI/SortButtons.tsx
@@ -9,11 +9,26 @@ interface Sort {
   sortValues: string[];
 }
 
+const filterHandler = (orderValue: string) => {
+  console.log(orderValue); // 정렬 관련 서버 기능 구현 후 동작 처리하겠습니다.
+};
+
 const SortButtons = ({ sortValues }: Sort) => {
   return (
     <SSortContainer>
       {sortValues.map((value, idx) => (
-        <li key={idx}>{value}</li>
+        <li
+          key={idx}
+          role="presentation"
+          onClick={() => {
+            filterHandler(value);
+          }}
+          onKeyDown={() => {
+            filterHandler(value);
+          }}
+        >
+          {value}
+        </li>
       ))}
     </SSortContainer>
   );
