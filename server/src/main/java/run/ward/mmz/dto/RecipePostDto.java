2a615fb72a824dd37992c1290f14cbff9206b9a7
ward <bonuswon@gmail.com>
2022-09-27 21:49:27 +0900

    feat: postDto serialize 삭제 #31

--- server/src/main/java/run/ward/mmz/dto/RecipePostDto.java
+++ server/src/main/java/run/ward/mmz/dto/RecipePostDto.java
@@ -1,20 +1,15 @@
 package run.ward.mmz.dto;
 
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import run.ward.mmz.domain.account.Account;
-import run.ward.mmz.domain.file.Files;
-import run.ward.mmz.domain.post.*;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 
 @Getter
 @NoArgsConstructor
-public class RecipePostDto implements Serializable {
+public class RecipePostDto{
 
 
     private String title;
