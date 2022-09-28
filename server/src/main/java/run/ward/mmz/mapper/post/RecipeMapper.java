a344fbc89ed9d753486866d5d75056ed4df4e145
ward <bonuswon@gmail.com>
2022-09-28 17:19:17 +0900

    feat: info Dto 작성 및 response 구현 #31

--- server/src/main/java/run/ward/mmz/mapper/post/RecipeMapper.java
+++ server/src/main/java/run/ward/mmz/mapper/post/RecipeMapper.java
@@ -5,6 +5,7 @@ import run.ward.mmz.domain.account.Account;
 import run.ward.mmz.domain.file.Files;
 import run.ward.mmz.domain.post.*;
 import run.ward.mmz.dto.request.RecipePostDto;
+import run.ward.mmz.dto.respones.RecipeInfoDto;
 import run.ward.mmz.dto.respones.RecipeResponseDto;
 
 import java.util.List;
@@ -12,4 +13,6 @@ import java.util.List;
 public interface RecipeMapper {
     Recipe toEntity(Account owner, RecipePostDto recipePostDto, Files imgThumbNail, List<Ingredient> ingredients, List<Direction> directionList);
     RecipeResponseDto toResponseDto(Recipe recipe, List<Tag> tagList);
+    RecipeInfoDto toInfoDto(Recipe recipe, List<Tag> tagList);
+
 }
