a62998cc1a114eea4feed7096edfbca6cb853850
ward <bonuswon@gmail.com>
2022-09-30 03:45:07 +0900

    feat : cors 설정 #31

--- server/src/main/java/run/ward/mmz/web/config/WebMvcConfig.java
+++ server/src/main/java/run/ward/mmz/web/config/WebMvcConfig.java
@@ -19,8 +19,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080", "http://localhost:8081")
-                .allowCredentials(false)
+                .allowedOrigins("*")
+                .allowCredentials(true)
                 .allowedHeaders("*")
                 .allowedMethods("*");
     }
