97ae59fa9e7617a8b0dc3aaa2ceb0008f3cc8d8f
ward <bonuswon@gmail.com>
2022-09-29 12:16:04 +0900

    feat: account service 추가  #31

--- server/src/main/java/run/ward/mmz/service/impl/BookmarkServiceImpl.java
+++ server/src/main/java/run/ward/mmz/service/impl/BookmarkServiceImpl.java
@@ -1,13 +1,32 @@
 package run.ward.mmz.service.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import run.ward.mmz.domain.account.Account;
 import run.ward.mmz.domain.post.Recipe;
+import run.ward.mmz.repository.AccountRepository;
+import run.ward.mmz.repository.BookmarkRepository;
+import run.ward.mmz.repository.RecipeRepository;
+import run.ward.mmz.service.AccountService;
 import run.ward.mmz.service.BookmarkService;
+import run.ward.mmz.service.RecipeService;
 
+@Service
+@RequiredArgsConstructor
 public class BookmarkServiceImpl implements BookmarkService {
+
+    private final BookmarkRepository bookmarkRepository;
+    private final RecipeService recipeService;
+    private final AccountService accountService;
 
     @Override
     public void setBookmarked(Long recipeId, Long accountId) {
+
+        Recipe recipe = recipeService.findById(recipeId);
+        Account account = accountService.findById(accountId);
+
+
 
     }
 
