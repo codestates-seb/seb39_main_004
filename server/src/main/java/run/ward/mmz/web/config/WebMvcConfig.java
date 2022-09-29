c76be3665f673d8706eacef934e8710c3554507e
ward <bonuswon@gmail.com>
2022-09-30 03:23:35 +0900

    feat : cors 설정 #31

--- server/src/main/java/run/ward/mmz/web/config/WebMvcConfig.java
+++ server/src/main/java/run/ward/mmz/web/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +15,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Value("${file.path}")
     private String uploadFolder;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("*");
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
