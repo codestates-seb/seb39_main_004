eceb4b9e7b1935190bb1d63761e420fa4878dec3
string_main <yhj2756@naver.com>
2022-10-01 02:55:32 +0900

    feat: 로그인 기능 구현 (#57)

--- client/src/components/Login/LoginForm.tsx
+++ client/src/components/Login/LoginForm.tsx
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 import { Button, StyledInput } from "../../components/CommonUI";
 import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+import qs from "qs";
+import { useNavigate } from "react-router-dom";
 
 const SForm = styled.form``;
 
@@ -20,6 +23,8 @@ type Inputs = {
 };
 
 const LoginForm = () => {
+  const navigate = useNavigate();
+
   const {
     register,
     handleSubmit,
@@ -27,7 +32,32 @@ const LoginForm = () => {
   } = useForm<Inputs>();
 
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    console.log(data);
+    console.log(data.email, data.password);
+
+    const axiosConfig = {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      withCredentials: true,
+    };
+
+    const axiosBody = {
+      username: data.email,
+      password: data.password,
+    };
+
+    axios
+      .post("/api/v1/auth/login", qs.stringify(axiosBody), axiosConfig)
+      .then((response) => {
+        console.log(response); // 구현 완료 후 제거
+
+        if (response.status === 200) {
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   return (
