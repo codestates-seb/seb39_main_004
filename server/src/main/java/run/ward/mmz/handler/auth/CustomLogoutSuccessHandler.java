462074a1937251e598755dcaa8d6ff1e46becf94
ward <bonuswon@gmail.com>
2022-10-02 21:20:59 +0900

    feat : 로그아웃 service di추가 #83

--- server/src/main/java/run/ward/mmz/handler/auth/CustomLogoutSuccessHandler.java
+++ server/src/main/java/run/ward/mmz/handler/auth/CustomLogoutSuccessHandler.java
@@ -1,13 +1,17 @@
 package run.ward.mmz.handler.auth;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Service;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Service
+@RequiredArgsConstructor
 public class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
 
     @Override
