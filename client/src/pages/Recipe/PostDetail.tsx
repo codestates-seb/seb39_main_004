5eed9ca80f5dec313c7d3506f1fcf6c4b888f094
dulce04 <aocl333@nate.com>
2022-10-04 15:14:36 +0900

    레시피 조회 페이지 CSS (#67)

--- client/src/pages/Recipe/PostDetail.tsx
+++ client/src/pages/Recipe/PostDetail.tsx
@@ -2,29 +2,37 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import styled from "styled-components";
-import { Tag } from "../../components/CommonUI";
 import UserInfo from "../../components/RecipeDetail/UserInfo";
 import IngredientItem from "../../components/RecipeDetail/IngredientInfo";
 import CategoryInfo from "../../components/RecipeDetail/CategoryInfo";
 import RecipeStep from "../../components/RecipeDetail/RecipeStep";
 import RepleContainer from "../../components/RecipeDetail/RepleContainer";
 import { IPostResponceProps } from "../../types/interface";
+import recipeLogo from "../../assets/images/Recipe/recipeLogo.svg";
 
 const SThumbNailContainer = styled.img`
   width: 100%;
   height: 400px;
   object-fit: cover;
 `;
+
 const SUserContainer = styled.div``;
 
-const SFollowBtn = styled.div`
-  width: 58px;
-  margin: 15px auto;
-  text-align: center;
-  padding: 7px 0;
-  background-color: #000;
-  color: #fff;
-  font-size: 0.7rem;
+const SLogoRecipe = styled.img`
+  position: absolute;
+  top: -50px;
+  right: 0;
+  left: 0;
+  margin: 0 auto;
+  display: block;
+`;
+
+const STopContainer = styled.div`
+  position: relative;
+  background-color: #f2f1ea;
+  padding: 30px;
+  margin-top: 8rem;
+  margin-bottom: 30px;
 `;
 
 const SHeaderContainer = styled.div`
@@ -32,7 +40,9 @@ const SHeaderContainer = styled.div`
   padding-bottom: 20px;
   text-align: center;
   h1 {
-    font-size: 2rem;
+    font-size: 2.1rem;
+    padding-top: 30px;
+    display: inline-block;
   }
 `;
 
@@ -40,25 +50,18 @@ const SDesc = styled.p`
   position: relative;
   padding-top: 15px;
   color: var(--gray);
-  font-size: 1rem;
+  font-size: 1.1rem;
 `;
 
 const IngredientContainer = styled.div`
   width: 100%;
   h4 {
     color: var(--deep-green);
-    font-size: 1.3rem;
+    font-size: 1.5rem;
     font-weight: bold;
     padding-bottom: 20px;
     padding-left: 20px;
   }
-`;
-
-const STagContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-content: center;
-  gap: 3px;
 `;
 
 const STabMenu = styled.ul`
@@ -69,15 +72,26 @@ const STabMenu = styled.ul`
   align-items: center;
   list-style: none;
   margin-bottom: 50px;
-  border-bottom: 1px solid var(--pale-green);
+  border-bottom: 3px solid var(--pale-green);
   .submenu {
+    width: 150px;
     padding: 10px 30px;
+    font-size: 1.2rem;
+    text-align: center;
     cursor: pointer;
   }
   .focused {
-    border-top: 3px solid var(--deep-gray);
-    border-right: 1px solid var(--pale-gray);
-    border-left: 1px solid var(--pale-gray);
+    position: relative;
+    :after {
+      content: "";
+      position: absolute;
+      right: 0;
+      left: 0;
+      bottom: -3px;
+      display: block;
+      width: 150px;
+      border-bottom: 3px solid var(--red);
+    }
   }
 `;
 
@@ -86,7 +100,7 @@ const SRecipeInfoContainer = styled.div`
   grid-template-columns: 300px auto;
   column-gap: 70px;
   flex-grow: 1;
-  padding: 70px 0;
+  padding: 50px 0 30px;
 `;
 
 const SRecipeStepContainer = styled.div``;
@@ -100,6 +114,7 @@ const RecipeDetail = () => {
     getData();
   }, []);
 
+  console.log(data);
   const getData = async () => {
     const { data } = await axios.get(`/api/v1/recipe/${id}`);
     setData([data.data]);
@@ -142,35 +157,34 @@ const RecipeDetail = () => {
               imgProfileUrl={data[0].owner.imgProfileUrl}
             />
           </SUserContainer>
-          <SFollowBtn>follow</SFollowBtn>
-          <SHeaderContainer>
-            <h1>{data[0].title}</h1>
-            <SDesc>{data[0].body}</SDesc>
-          </SHeaderContainer>
-          <STagContainer>
-            {data[0].tags.map((i) => (
-              <Tag key={i.id} name={i.name} />
-            ))}
-          </STagContainer>
-          <SRecipeInfoContainer>
-            <CategoryInfo
-              stars={data[0].stars}
-              views={data[0].views}
-              createDate={data[0].createDate}
-            />
-            <IngredientContainer>
-              <h4>필요한 재료</h4>
-              {data[0].ingredients.map((i) => (
-                <IngredientItem
-                  key={i.index}
-                  index={i.index}
-                  name={i.name}
-                  amount={i.amount}
-                  isEssential={i.isEssential}
-                />
-              ))}
-            </IngredientContainer>
-          </SRecipeInfoContainer>
+          <STopContainer>
+            <SLogoRecipe src={recipeLogo} alt="recipeLogo"></SLogoRecipe>
+            <SHeaderContainer>
+              <h1>{data[0].title}</h1>
+              <SDesc>{data[0].body}</SDesc>
+            </SHeaderContainer>
+            <SRecipeInfoContainer>
+              <CategoryInfo
+                stars={data[0].stars}
+                views={data[0].views}
+                createDate={data[0].createDate}
+                tags={data[0].tags}
+                category={data[0].category}
+              />
+              <IngredientContainer>
+                <h4>필요한 재료</h4>
+                {data[0].ingredients.map((i) => (
+                  <IngredientItem
+                    key={i.index}
+                    index={i.index}
+                    name={i.name}
+                    amount={i.amount}
+                    essential={i.essential}
+                  />
+                ))}
+              </IngredientContainer>
+            </SRecipeInfoContainer>
+          </STopContainer>
           <STabMenu>
             {menuArr.map((item) => {
               return (
