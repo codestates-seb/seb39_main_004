e54453ea21cb46da237454c2571e4b11669d970c
dulce04 <aocl333@nate.com>
2022-10-06 19:04:33 +0900

    feat: 탑버튼 버그 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -37,6 +37,11 @@ const STopBtn = styled.img`
   right: 45px;
   bottom: 60px;
   cursor: pointer;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 50px;
+    right: 10px;
+    bottom: 30px;
+  }
 `;
 
 const Main = () => {
@@ -48,8 +53,8 @@ const Main = () => {
   const { sessionStatus } = useAppSelector((state) => state.user);
 
   const [page, setPage] = useState(1); // 현재 페이지
-  const [load, setLoad] = useState<boolean>(true); // 로딩 스피너 상태
   const [showButton, setShowButton] = useState<boolean>(false); // 탑버튼
+  const [load, setLoad] = useState<boolean>(true); // 로딩 스피너 상태
   const preventRef = useRef(true); // 옵저버 중복 실행 방지
   const obsRef = useRef(null); //observer Element
   const endRef = useRef(false); //모든 글 로드 확인
