922a8305c22b2306daf306bd068d614c663bbedb
ward <bonuswon@gmail.com>
2022-10-10 23:49:19 +0900

    feat : 섬네일 이미지 변경안한 경우에도 동작 추가

--- server/src/main/java/run/ward/mmz/web/controller/v1/post/RecipeController.java
+++ server/src/main/java/run/ward/mmz/web/controller/v1/post/RecipeController.java
@@ -151,13 +151,17 @@ public class RecipeController {
         List<Direction> directionList = directionService.updateAll(directionPostDtoList, filesDtoList, recipeId);
         recipeService.deleteAllDirection(recipeId);
 
+
         Files imgThumbNailFile = filesMapper.fileDtoToImage(fileHandler.parseFileInfo(imgThumbNail, ImageType.EXTENSIONS));
+
         List<Ingredient> ingredients = ingredientMapper.toEntity(recipePostDto.getIngredients());
+
         List<Tag> tags = tagMapper.toEntity(recipePostDto.getTags());
         recipeService.deleteAllIngredient(recipeId);
 
         Recipe recipe = recipeMapper.toEntity(findUser, recipePostDto, imgThumbNailFile, ingredients, directionList);
         recipe = recipeService.update(recipeId, recipe);
+
         recipeService.deleteAllRecipeTag(recipeId);
         tagService.saveAll(tags);
         recipeTagService.save(tags, recipe);
