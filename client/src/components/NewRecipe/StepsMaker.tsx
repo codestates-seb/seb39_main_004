daff65e64b151f662561c1d356844154d67b27ea
JoeunNAL <euji42@gmail.com>
2022-10-11 21:25:02 +0900

    feat: 마이페이지 사용자 프로필 변경 추가(#184)

--- client/src/components/NewRecipe/StepsMaker.tsx
+++ client/src/components/NewRecipe/StepsMaker.tsx
@@ -14,46 +14,6 @@ const StepsMaker = ({
     isUploaded: false,
   };
 
-  // let initialValue;
-  // directDatas === undefined
-  //   ? (initialValue = [basicForm])
-  //   : (initialValue = directDatas);
-  // const [steps, setSteps] = useState<IStepValues[]>(initialValue);
-
-  // console.log(steps);
-
-  // const addDirectionsHander = () => {
-  //   if (setEditResponse && editResponse) {
-  //     const newRes = { ...editResponse };
-  //     console.log("핸들러", newRes);
-  //     if (newRes.directions) {
-  //       const lastStepId = newRes.directions.slice(-1)[0].index;
-  //       newRes.directions.push({
-  //         index: lastStepId + 1,
-  //         imgDirectionUrl: "",
-  //         body: "",
-  //       });
-  //     }
-  //     console.log("newRes", newRes);
-  //     setEditResponse({ ...newRes });
-  //   }
-  // };
-
-  //   const lastStep = steps.slice(-1)[0];
-  //   if (lastStep) {
-  //     basicForm.index = lastStep.index + 1;
-  //   }
-  //   setSteps([...steps, basicForm]);
-
-  // const initialValue = resDirecttions ? resDirecttions : [basicForm];
-  // console.log("directDatas", directDatas);
-  // console.log("steps", steps);
-  // if (editResponse && editResponse.directions) {
-  //   // editResponse.directions((el) => {
-  //   console.log("요소", editResponse.directions);
-  //   // });
-  // }
-
   const addDirectionsHander = () => {
     const lastStep = directDatas.slice(-1)[0];
     if (lastStep) {
--- client/src/components/NewRecipe/ThumbNailUploader.tsx
+++ client/src/components/NewRecipe/ThumbNailUploader.tsx
@@ -10,6 +10,21 @@ const SImgInputContainer = styled.div`
   flex-direction: column;
 `;
 
+const SUserImg = styled.img`
+  display: inline-block;
+  overflow: hidden;
+  position: relative;
+  box-sizing: border-box;
+  background-color: var(--pale-gray);
+  width: 150px;
+  height: 150px;
+  border-radius: 50%;
+  @media ${({ theme }) => theme.device.mobile} {
+    width: 100px;
+    height: 100px;
+  }
+`;
+
 const SImg = styled.img`
   width: 430px;
   height: 275px;
@@ -28,6 +43,7 @@ const SImgInput = styled.input`
 `;
 
 const ThumbNailUploader = ({
+  isMypage,
   resThumbNailImgUrl,
   setThumbNail,
 }: IThumbNailProps) => {
@@ -46,23 +62,41 @@ const ThumbNailUploader = ({
 
   return (
     <SImgInputContainer>
-      <SLable htmlFor="img">
-        썸네일
-        <RequireMark />
-      </SLable>
-      <SImg
-        src={
-          fileURL !== `${process.env.PUBLIC_URL}/assets/${undefined}`
-            ? fileURL
-            : defaultImg
-        }
-        alt={fileURL ? fileURL.slice(8) : "undefined"}
-        onClick={() => {
-          if (imgUploadInput.current) {
-            imgUploadInput.current.click();
+      {isMypage ? (
+        <SUserImg
+          src={
+            fileURL !== `${process.env.PUBLIC_URL}/assets/${undefined}`
+              ? fileURL
+              : `${process.env.PUBLIC_URL}/assets/${resThumbNailImgUrl}`
           }
-        }}
-      ></SImg>
+          onClick={() => {
+            if (imgUploadInput.current) {
+              imgUploadInput.current.click();
+            }
+          }}
+        ></SUserImg>
+      ) : (
+        <>
+          <SLable htmlFor="img">
+            썸네일
+            <RequireMark />
+          </SLable>
+          <SImg
+            src={
+              fileURL !== `${process.env.PUBLIC_URL}/assets/${undefined}`
+                ? fileURL
+                : defaultImg
+            }
+            alt={fileURL ? fileURL.slice(8) : "undefined"}
+            onClick={() => {
+              if (imgUploadInput.current) {
+                imgUploadInput.current.click();
+              }
+            }}
+          ></SImg>
+        </>
+      )}
+
       <SImgInput
         type="file"
         id="img"
