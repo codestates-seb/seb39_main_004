b0cda574a71913b067cae718170c88dc9c9099bd
JoeunNAL <euji42@gmail.com>
2023-03-21 00:22:33 +0900

    style: recipeSlice ESlint any타입 임시 허용 주석 추가

--- client/src/redux/slices/recipeSlice.ts
+++ client/src/redux/slices/recipeSlice.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { TypeOfInputSectionsWithRemoveButton } from "../../types/type";
